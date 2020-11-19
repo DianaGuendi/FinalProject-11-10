500e8c9100abedfcf5bfd0d618f878a5ab4b4183
aloza019 <andresafl@hotmail.com>
2020-11-19 03:10:30 +0000

    navbarv2

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -10,69 +10,72 @@ export const Navbart = () => {
 	const handleShow = () => setShow(true);
 	return (
 		<div className="container-fluid">
-			<div className="login-but row">
-				<Button className="login-button" variant="primary" onClick={handleShow}>
-					Login
-				</Button>
-
-				<Modal show={show} onHide={handleClose}>
-					<Modal.Header closeButton>
-						<Modal.Title>Login</Modal.Title>
-					</Modal.Header>
-					<InputGroup className="userlog mb-3">
-						<FormControl placeholder="username" />
-					</InputGroup>
-					<InputGroup className="userlog mb-3">
-						<FormControl placeholder="password" />
-					</InputGroup>
-					{/* <Modal.Body>Woohoo</Modal.Body> */}
-					<Modal.Footer>
-						<Button className="login-button" variant="primary" onClick={handleClose}>
-							Login
-						</Button>
-					</Modal.Footer>
-				</Modal>
-			</div>
-			<div className="logomit">
-				<img id="logo" src={mitlog} />
-			</div>
-			<div className="info-nav ">
-				<div className="phones">
-					<i className="fas fa-phone col-1" />
-					<div className="col-11">
-						<p className="card-info">305-902-1999</p>
+			<div className="logoinfolog row">
+				<div className="logomit col-3">
+					<img id="logo" src={mitlog} />
+				</div>
+				<div className="info-nav col-6">
+					<div className="phones">
+						<i className="fas fa-phone col-1" />
+						<div className="col-11">
+							<p className="card-info">305-902-1999</p>
+						</div>
+					</div>
+					<br />
+					<div className="email">
+						<i className="far fa-envelope col-1" />
+						<div>
+							<a href="mailto: alozano@mitlg.com" className="card-info col-11">
+								alozano@mitlg.com
+							</a>
+						</div>
 					</div>
 				</div>
-				<br />
-				<div className="email">
-					<i className="far fa-envelope col-1" />
-					<div>
-						<a href="mailto: alozano@mitlg.com" className="card-info col-11">
-							alozano@mitlg.com
-						</a>
-					</div>
+				<div className="login-but col-3">
+					<Button className="login-button" variant="primary" onClick={handleShow}>
+						Login
+					</Button>
+
+					<Modal show={show} onHide={handleClose}>
+						<Modal.Header closeButton>
+							<Modal.Title>Login</Modal.Title>
+						</Modal.Header>
+						<InputGroup className="userlog mb-3">
+							<FormControl placeholder="username" />
+						</InputGroup>
+						<InputGroup className="userlog mb-3">
+							<FormControl placeholder="password" />
+						</InputGroup>
+						{/* <Modal.Body>Woohoo</Modal.Body> */}
+						<Modal.Footer>
+							<Button className="login-button" variant="primary" onClick={handleClose}>
+								Login
+							</Button>
+						</Modal.Footer>
+					</Modal>
 				</div>
 			</div>
-
-			<Navbar collapseOnSelect expand="lg ">
-				<Navbar.Toggle aria-controls="responsive-navbar-nav ml-auto" />
-				<Navbar.Collapse id="responsive-navbar-nav ml-auto">
-					<Nav className="mr-auto">
-						<Nav.Link id="navlink" href="/">
-							Home
-						</Nav.Link>
-						<Nav.Link id="navlink" href="/About">
-							About
-						</Nav.Link>
-						<Nav.Link id="navlink" href="/Services">
-							Services
-						</Nav.Link>
-						<Nav.Link id="navlink" href="/Tracking">
-							Tracking
-						</Nav.Link>
-					</Nav>
-				</Navbar.Collapse>
-			</Navbar>
+			<div className="navbar-alig">
+				<Navbar collapseOnSelect expand="lg ">
+					<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+					<Navbar.Collapse id="responsive-navbar-nav ">
+						<Nav className="justify-content-center">
+							<Nav.Link id="navlink" href="/">
+								Home
+							</Nav.Link>
+							<Nav.Link id="navlink" href="/About">
+								About
+							</Nav.Link>
+							<Nav.Link id="navlink" href="/Services">
+								Services
+							</Nav.Link>
+							<Nav.Link id="navlink" href="/Tracking">
+								Tracking
+							</Nav.Link>
+						</Nav>
+					</Navbar.Collapse>
+				</Navbar>
+			</div>
 		</div>
 	);
 };
